7c4264c0811c7e2c43b984332107ce3a98fba338
Aryan Padmakar Shirsat <aryanshirsat357@gmail.com>
2024-01-26 19:48:15 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Studentdashboard from './pages/Studentdashboard';
+function App() {
+  return (
+    <>
+      <Studentdashboard/>
+    </>
+  );
+}
+
+export default App;
